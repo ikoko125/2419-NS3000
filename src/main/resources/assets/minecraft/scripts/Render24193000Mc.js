265fefe773864b0038064318e583ad2cb1508c32
Siro_256 <siro@siro256.dev>
2022-01-25 03:54:53 +0900

    Remove unnecessary imports

--- src/main/resources/assets/minecraft/scripts/Render24193000Mc.js
+++ src/main/resources/assets/minecraft/scripts/Render24193000Mc.js
@@ -1,18 +1,7 @@
 //このスクリプトはフリーなので好きなだけコピったり複製して改造してOKです
 
 var renderClass = "jp.ngt.rtm.render.VehiclePartsRenderer";
-importPackage(Packages.org.lwjgl.opengl);
-importPackage(Packages.jp.ngt.rtm.render);
-importPackage(Packages.jp.ngt.ngtlib.math);
 
-//先頭車から移植してるので使わないのもある↓
-importPackage(Packages.net.minecraft.tileentity); //TileEntitySign
-importPackage(Packages.jp.ngt.ngtlib.util); //NGTUtil NGTUtilClient
-importPackage(Packages.org.lwjgl.util.vector); //Vector3f
-importPackage(Packages.jp.ngt.ngtlib.renderer); //NGTTessellator
-importPackage(Packages.jp.ngt.ngtlib.io); //NGTLog
-importPackage(Packages.net.minecraft.util); //ResourceLocation
-importPackage(Packages.org.lwjgl.input); //Keyboard
 importPackage(Packages.com.github.ikoko125.banetsu2419_ns3000.model);
 importPackage(Packages.com.github.ikoko125.banetsu2419_ns3000.render);
 

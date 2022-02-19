c58cd9795dd6c27296f6b82513b0f474e21544c9
Siro_256 <siro@siro256.dev>
2022-02-19 09:31:08 +0900

    Remove unnecessary imports

--- src/main/resources/assets/minecraft/scripts/RenderART500Tc.js
+++ src/main/resources/assets/minecraft/scripts/RenderART500Tc.js
@@ -1,7 +1,5 @@
 var renderClass = "jp.ngt.rtm.render.VehiclePartsRenderer";
-importPackage(Packages.org.lwjgl.opengl);
-importPackage(Packages.jp.ngt.rtm.render);
-importPackage(Packages.jp.ngt.ngtlib.math);
+
 importPackage(Packages.com.github.ikoko125.banetsu2419_ns3000.model);
 importPackage(Packages.com.github.ikoko125.banetsu2419_ns3000.render);
 
